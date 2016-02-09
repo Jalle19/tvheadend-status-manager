84dad4a99127de8b7fdb0b7055bf44d950288fb6
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-02-09 22:00:54 +0200

    provision some swap space so Composer doesn't complain and die

---
+++ provisioning/01-configure-system.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+{
+	# enable swap (disable first to prevent errors during re-provisioning)
+    swapoff /tmp/swap
+    dd if=/dev/zero of=/tmp/swap bs=1M count=512
+    mkswap /tmp/swap
+    swapon /tmp/swap
+} > /dev/null 2>&1
