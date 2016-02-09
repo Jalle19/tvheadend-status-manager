987e9a341f20d3768cb98b4fdd0967ef895d4739
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-02-09 21:45:18 +0200

    remove redundant phpdoc

--- src/cli/Jalle19/StatusManager/InstanceStatus.php
+++ src/cli/Jalle19/StatusManager/InstanceStatus.php
@@ -54,19 +54,15 @@ class InstanceStatus implements \JsonSerializable
 
 
 	/**
-	 * (PHP 5 &gt;= 5.4.0)<br/>
-	 * Specify data which should be serialized to JSON
-	 * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-	 * @return mixed data which can be serialized by <b>json_encode</b>,
-	 * which is a value of any type other than a resource.
+	 * @inheritdoc
 	 */
-	function jsonSerialize()
+	public function jsonSerialize()
 	{
 		return [
-			'instanceName'  => $this->_instanceName,
-			'inputs'        => $this->_inputs,
-			'subscriptions' => $this->_subscriptions,
-			'connections'   => $this->_connections,
+			'instanceName'             => $this->_instanceName,
+			'inputs'                   => $this->_inputs,
+			'subscriptions'            => $this->_subscriptions,
+			'connections'              => $this->_connections,
 		];
 	}
 
