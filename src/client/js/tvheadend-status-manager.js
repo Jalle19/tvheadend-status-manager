caea81074c16ca05dbb139cb63f1f99116470d69
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-02-10 23:08:55 +0200

    draw realtime subscription bandwidth graphs

--- src/client/js/tvheadend-status-manager.js
+++ src/client/js/tvheadend-status-manager.js
@@ -1,21 +1,95 @@
-var statusManagerApp = angular.module('statusManagerApp', []);
+var statusManagerApp = angular.module('statusManagerApp', [
+  'angular-flot',
+  'ngLodash'
+]);
 
 /**
  * Main controller
  */
-statusManagerApp.controller('StatusController', function($scope) {
-  // Keeps track of the connection status
+statusManagerApp.controller('StatusController', function($scope, lodash) {
+  /**
+   * @type {number} the maximum amount of data points to display in graphs
+   */
+  var MAXIMUM_GRAPH_DATAPOINTS = 15;
+
+  /**
+   * @type {boolean} whether the client is connected to the server
+   */
   $scope.connected = false;
 
-  // Date formatting
+  /**
+   * @type {Array} the instances that are monitored
+   */
+  $scope.instances = [];
+
+  /**
+   * @type {string} the date format string to use
+   */
   $scope.dateFormat = 'yyyy-MM-dd HH:mm:ss';
+
+  /**
+   * @type {Array} the subscription chart data
+   */
+  $scope.subscriptionChartData = [];
+
+  /**
+   * The options for the subscription chart
+   */
+  $scope.subscriptionChartOptions = {
+    xaxis: {
+      fill: true,
+      show: false
+    },
+    yaxis: {
+      axisLabel: "kbps",
+      axisLabelFontSizePixels: 11
+    },
+    series: {
+      stack: true,
+      lines: {
+        fill: true
+      }
+    }
+  };
+
+  /**
+   * Formats the specified date
+   * @param date
+   * @returns {Date}
+   */
   $scope.formatDate = function(date) {
     return new Date(date * 1000);
   };
 
-  // Bitrate formatting
+  /**
+   * Formats the specified bitrate
+   * @param bps
+   * @returns {number}
+   */
   $scope.formatBitrate = function(bps) {
     return Math.round(bps * 8 / 1024);
+  };
+
+  /**
+   *
+   * @param subscriptionId
+   * @returns {*}
+   */
+  var getSubscriptionDataset = function(instanceName, subscriptionId) {
+    return lodash.find($scope.subscriptionChartData[instanceName], function(s) {
+      return s.subscriptionId === subscriptionId;
+    });
+  };
+
+  /**
+   *
+   * @param instanceName
+   * @param subscriptionId
+   */
+  var removeSubscriptionDataset = function(instanceName, subscriptionId) {
+    lodash.remove($scope.subscriptionChartData[instanceName], function(s) {
+      return s.subscriptionId === subscriptionId;
+    });
   };
 
   // Connect to the server
@@ -30,40 +104,61 @@ statusManagerApp.controller('StatusController', function($scope) {
   };
 
   /**
-   * Parses incoming messages
-   * @param event
-   */
-  websocket.onmessage = function(event) {
-    var instances = JSON.parse(event.data).instances;
-    var scopeInstances = [];
-
-    instances.forEach(function(instance) {
-      var name = instance.instanceName;
-      var inputs = instance.inputs;
-      var subscriptions = instance.subscriptions;
-      var connections = instance.connections;
-
-      scopeInstances.push({
-        'name': name,
-        'inputs': inputs,
-        'subscriptions': subscriptions,
-        'connections': connections
-      });
-    }
-
-    // Update scope
-    $scope.instances = scopeInstances;
-    $scope.$apply();
-  };
-
-  /**
    * Called when the connection is lost
    */
   websocket.onclose = function() {
     $scope.connected = false;
-
-    // Clear all instances
     $scope.instances = [];
+    $scope.subscriptionChartData = [];
+    $scope.$apply();
+  };
+
+  /**
+   * Parses incoming messages
+   * @param event
+   */
+  websocket.onmessage = function(event) {
+    $scope.instances = JSON.parse(event.data).instances;
+
+    $scope.instances.forEach(function(instance) {
+      var name = instance.instanceName;
+
+      // Create a data set for the instance
+      if (!(name in $scope.subscriptionChartData)) {
+        $scope.subscriptionChartData[name] = [];
+      }
+
+      // Update the data set for each subscription
+      instance.subscriptions.forEach(function(subscription) {
+        var dataset = getSubscriptionDataset(name, subscription.id);
+
+        // Create a new data set for new subscriptions
+        if (dataset === undefined) {
+          $scope.subscriptionChartData[name].push({
+            subscriptionId: subscription.id,
+            label: subscription.channel,
+            data: []
+          });
+        }
+
+        // Append the data
+        var bitrate = $scope.formatBitrate(subscription.in);
+
+        dataset = getSubscriptionDataset(name, subscription.id);
+        dataset.data.push([Date.now(), bitrate]);
+
+        // Limit the data
+        if (dataset.data.length > MAXIMUM_GRAPH_DATAPOINTS)
+          dataset.data.shift();
+      });
+
+      // Handle subscription state changes
+      instance.subscriptionStateChanges.forEach(function(stateChange) {
+        if (stateChange.state === 'stopped') {
+          removeSubscriptionDataset(name, stateChange.subscriptionId);
+        }
+      });
+    });
 
     $scope.$apply();
   };
