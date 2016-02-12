9905435a47e0386426a255faa587d5cba0c602a8
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-02-12 11:09:15 +0200

    add database schema file

--- sql/tvheadend_status_manager.sql
+++ sql/tvheadend_status_manager.sql
@@ -55,15 +55,22 @@ DROP TABLE IF EXISTS [input];
 CREATE TABLE [input]
 (
     [uuid] VARCHAR(255) NOT NULL,
+<<<<<<< HEAD
     [instance_name] VARCHAR(255) NOT NULL,
+=======
+>>>>>>> 868ad65... add database schema file
     [started] TIMESTAMP NOT NULL,
     [input] VARCHAR(255) NOT NULL,
     [network] VARCHAR(255) NOT NULL,
     [mux] VARCHAR(255) NOT NULL,
     [weight] INTEGER NOT NULL,
     PRIMARY KEY ([uuid]),
+<<<<<<< HEAD
     UNIQUE ([uuid]),
     FOREIGN KEY ([instance_name]) REFERENCES [instance] ([name])
+=======
+    UNIQUE ([uuid])
+>>>>>>> 868ad65... add database schema file
 );
 
 -----------------------------------------------------------------------
