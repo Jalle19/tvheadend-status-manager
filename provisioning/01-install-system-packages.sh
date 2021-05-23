892e9f47efdfd02fab8918b85607ac37401ae869
Sam Stenvall <sam.stenvall@nitor.com>
2021-05-23 11:33:32 +0300

    Upgrade PHP to 7.4 and Node.js to 14.x

--- provisioning/01-install-system-packages.sh
+++ provisioning/01-install-system-packages.sh
@@ -4,13 +4,10 @@
 	add-apt-repository -y ppa:ondrej/php
 	
 	apt-get update
-	apt-get -y install php7.0-cli php7.0-xdebug php7.0-xml php7.0-sqlite3 curl git-core unzip
-	
-	# clean up from older nodejs installations
-	apt-get -y remove --purge nodejs* npm
+	apt-get -y install php7.4-cli php7.4-xdebug php7.4-xml php7.4-sqlite3 php7.4-curl php7.4-mbstring curl git-core unzip
 	
 	# install nodejs from repository
-	curl -sL https://deb.nodesource.com/setup_6.x | bash -
+	curl -sL https://deb.nodesource.com/setup_14.x | bash -
 	apt-get install -y nodejs
 
 	cd /vagrant
